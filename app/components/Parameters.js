480988d958bcc92d1b1cc08745ebda7b17e634af
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-08 16:58:42 +0100

    add: Ajout d'un curseur pour choisir le nombre de question (ne s'affiche pas)

--- app/components/Parameters.js
+++ app/components/Parameters.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import RangeCursor from './Cursor';
 
 export default function Parameters() {
     return (
         <View>
-          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Choisissez le nombre de question</Text>
+          <RangeCursor />
           <StatusBar style="auto" />
         </View>
     );
