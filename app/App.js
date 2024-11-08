7a4ce3f2718c31a9aeda9b4d3bd44d06471a87b5
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-08 16:47:18 +0100

    add: Ajout des composants Home et Parameters qui sont des Drawer, possibilité de les ouvrir avec des boutons

--- app/App.js
+++ app/App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Drawer } from 'react-native-drawer-layout';
+import { useNavigation } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './components/Home';
+import Parameters from './components/Parameters';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+const LeftDrawer = createDrawerNavigator();
+
+const LeftDrawerScreen = () => {
+  return (
+    <LeftDrawer.Navigator screenOptions={{ drawerPosition: 'left', headerShown: false  }}>
+      <LeftDrawer.Screen name="Home" component={Home} />
+    </LeftDrawer.Navigator>
+  );
+};
+
+const RightDrawer = createDrawerNavigator();
+
+const RightDrawerScreen = () => {
+  return (
+    <RightDrawer.Navigator
+      screenOptions={{ drawerPosition: 'right', headerShown: false }}
+    >
+      <RightDrawer.Screen name="HomeDrawer" component={LeftDrawerScreen} />
+    </RightDrawer.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RightDrawerScreen />
+    </NavigationContainer>
   );
 }
 
