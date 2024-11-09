44b5ce7dd1a233efaf9e1d51e6b6d705e24822d5
behrg <guillaume.behr3@etu.unistra.fr>
2024-11-09 21:24:20 +0100

    add : Support https serveur web.

--- web-server/server.js
+++ web-server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
+
+// Chemins vers vos certificats SSL (Let's Encrypt)
+const sslOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/luoja.fr/privkey.pem', 'utf8'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/luoja.fr/fullchain.pem', 'utf8'),
+};
 
 const app = express();
 const PORT = 4000;
 
-// Vérifiez que le dossier est bien servi
+// Servir les fichiers statiques à partir du dossier `dist`
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
 // Route pour renvoyer `index.html` pour toutes les autres requêtes
@@ -12,6 +20,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
+// Créer un serveur HTTPS sur le port 4000
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`🚀 Serveur HTTPS lancé sur https://localhost:${PORT}`);
 });
