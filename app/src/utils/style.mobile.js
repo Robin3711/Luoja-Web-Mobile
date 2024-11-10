ebcb69fe6cb9fe8f77dd5ed54a3eb77769c40a5b
behrg <guillaume.behr3@etu.unistra.fr>
2024-11-10 14:03:18 +0100

    add : Système de gestion de CSS suivant la plateforme.

---
+++ app/src/utils/style.mobile.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    homeScreen: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'white',
+    },
+    menuIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: 'white',
+    },
+    safeAreaView: {
+        flex: 1,
+    },
+});
