cc4fbf67e8dc78a1a7620d423e93a678c2bf2381
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-10 13:51:45 +0100

    add: Choix de la difficulté

--- app/components/Parameters.js
+++ app/components/Parameters.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import RangeCursor from './Cursor';
+import { useEffect, useState } from 'react';
+import RNPickerSelect from 'react-native-picker-select';
 
 export default function Parameters() {
+    const [Themes, setThemes] = useState([]);
+    const [difficulty, setDifficulty] = useState('none');
+    
+
+    const difficultyOptions = [
+        { label: 'Facile', value: 'easy' },
+        { label: 'Moyen', value: 'medium' },
+        { label: 'Difficile', value: 'hard' },
+    ];
     return (
         <View>
           <Text>Choisissez le nombre de question</Text>
           <RangeCursor />
+          <Text>Choisissez un thème</Text>
+          <Text>Choisissez le difficulté</Text>
+          <RNPickerSelect
+                onValueChange={(value) => setDifficulty(value)}
+                items={difficultyOptions}
+                value={difficulty}
+            />
           <Button title="Créer le quiz" onPress={() => console.log('Créer le quiz')} />
           <StatusBar style="auto" />
         </View>
