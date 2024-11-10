f8cecad4fdc9edce4c446ce59c25e0030a6f0756
behrg <guillaume.behr3@etu.unistra.fr>
2024-11-10 14:40:19 +0100

    update : Menu d'accueil.

--- app/src/screens/HomeScreen.js
+++ app/src/screens/HomeScreen.js
@@ -1,23 +1,15 @@
-import { View, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View } from 'react-native';
 import { getPlatformStyle } from '../utils/utils';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import Feather from '@expo/vector-icons/Feather';
+
+import DrawerButton from '../components/DrawerButton';
 import HomeScreenButtons from '../components/HomeScreenButtons';
 
 const styles = getPlatformStyle();
 
 export default function HomeScreen() {
-
-    const navigation = useNavigation();
-    
     return (
         <View style={styles.homeScreen}>
-            <View>
-                <TouchableOpacity style={styles.menuIcon} onPress={() => navigation.openDrawer()}>
-                    <Feather name="menu" size={50} color="black"/>
-                </TouchableOpacity>
-            </View>
+            <DrawerButton/>
             <HomeScreenButtons/>
         </View>
     );
