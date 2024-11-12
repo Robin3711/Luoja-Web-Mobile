99ce925dfcbc2c17a5d0d5737f04f1f52105d1c0
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-12 12:20:57 +0100

    Add: Peut ajouter l"identifiant de la partie et le sauvegarde

--- app/src/screens/ResumeScreen.js
+++ app/src/screens/ResumeScreen.js
@@ -1,10 +1,30 @@
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, Button } from 'react-native';
+import { getPlatformStyle } from "../utils/utils";
+import { getCurrentInfos } from "../utils/api";
+import { useState } from 'react';
+
+const styles = getPlatformStyle();
 
 export default function ResumeScreen() {
+    const [gameId, setGameId] = useState('');
+
+    const handleResumeGame = () => {
+        try
+        {
+            const infos = getCurrentInfos(gameId);
+            //navigation.navigate('QuizStack', { quizData: { quizId: gameId, questionCursor: infos.questionCursor } });
+
+        }
+        catch (error)
+        {
+            console.error(error);
+        }
+    }
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Reprenez votre partie</Text>
-            <TextInput placeholder="Identifiant de votre partie" />
+            <TextInput placeholder="Identifiant de votre partie" style={styles.textInput} onChangeText={setGameId} value={gameId}/>
+            <Button title="Reprendre" onPress={handleResumeGame} />
         </View>
     );
 }
