156e2ab8c5a02c038e4af30be2974a07a4e4f91d
guillaume-behr <guiguibehr@gmail.com>
2024-11-13 13:11:58 +0100

    add : platform console log

--- app/src/utils/utils.js
+++ app/src/utils/utils.js
@@ -1,6 +1,9 @@
 import { Platform } from 'react-native';
 
 export function getPlatformStyle() {
+
+    console.log('Platform.OS: ', Platform.OS);
+
     if (Platform.OS === 'web') {
         const styles = require('./style.web.js');
     } else {
