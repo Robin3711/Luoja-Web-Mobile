309ea399d74d23591776b706db51e15bcbf70932
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-20 15:07:26 +0100

    Add: Ajout d'un bouton pour jouer au quiz

--- app/src/components/QuizInformation.js
+++ app/src/components/QuizInformation.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { getPlatformStyle } from '../utils/utils';
+import { createParty } from '../utils/api';
+
+const styles = getPlatformStyle();
+
+
 
 export default function QuizInformation({quiz}) {
+    navigation = useNavigation();
+    const handleStartQuiz = () => {
+        createParty(quiz.id).then((party) => {
+            navigation.navigate('quizScreen', {quizId: party.id});
+        });
+    }
     return (
         <View style={styles.QuizInformationView}>
             <Text style={styles.QuizInformationText}>{quiz.title}</Text>
             <Text style={styles.QuizInformationText}>{quiz.category}</Text>
             <Text style={styles.QuizInformationText}>{quiz.difficulty}</Text>
             {/*<Text style={styles.QuizInformationText}>{quiz.questionCount}</Text>*/}
+            <Button title="Jouer" onPress={handleStartQuiz}/>
         </View>
     );
 }
