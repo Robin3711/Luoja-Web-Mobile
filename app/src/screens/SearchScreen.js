2722a51e6b3b0d3cb0d1e4581589015c3206648e
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-20 12:02:14 +0100

    Add: Peut chercher un titre et/ou un thème

--- app/src/screens/SearchScreen.js
+++ app/src/screens/SearchScreen.js
@@ -1,8 +1,22 @@
+import { Text, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { getPlatformStyle } from '../utils/utils';
+import ThemeSelector from '../components/ThemePicker';
+
+styles = getPlatformStyle();
 
 export default function SearchScreen() {
-  return (
-    <View>
-      <Text>Search Screen</Text>
+    const [theme, setTheme] = useState('none');
+    return (
+    <View style={styles.searchQuizView}>
+        <View style={styles.filterView}>
+            <Text style={styles.parametersText}>Titre</Text>
+            <TextInput style={styles.filterInput} placeholder="Rechercher un quiz par titre" />
+        </View>
+        <View style={styles.filterView}>
+            <ThemeSelector testID="themeSelector" value={theme} onValueChange={setTheme} />
+        </View>
+            
     </View>
   );
 }
