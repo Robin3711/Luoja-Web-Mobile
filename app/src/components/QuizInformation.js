15bb8a141b691fa559c835a5993dc6407178baad
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-20 13:53:08 +0100

    add: Ajout du composant contenant les informations d'une ligne de la liste des quiz

---
+++ app/src/components/QuizInformation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function QuizInformation({quiz}) {
+    return (
+        <View style={styles.QuizInformationView}>
+            <Text style={styles.QuizInformationText}>{quiz.title}</Text>
+            <Text style={styles.QuizInformationText}>{quiz.theme}</Text>
+            <Text style={styles.QuizInformationText}>{quiz.difficulty}</Text>
+            <Text style={styles.QuizInformationText}>{quiz.questionCount}</Text>
+        </View>
+    );
+}
