1e6839fbefff9ca42884357de33968fdfe74ee5c
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-21 12:03:15 +0100

    Update: Affiche le nom du thème et plus le numéros

--- app/src/components/QuizInformation.js
+++ app/src/components/QuizInformation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { createParty } from '../utils/api';
+import { getThemeName } from '../utils/utils';
 
 export default function QuizInformation({quiz}) {
     navigation = useNavigation();
@@ -10,10 +11,12 @@ export default function QuizInformation({quiz}) {
             navigation.navigate('quizScreen', {quizId: party.id});
         });
     }
+    console.log(quiz.category);
+    const themeName = getThemeName(parseInt(quiz.category));
     return (
         <View style={styles.QuizInformationView}>
             <Text style={styles.QuizInformationText}>{quiz.title}</Text>
-            <Text style={styles.QuizInformationText}>{quiz.category}</Text>
+            <Text style={styles.QuizInformationText}>{themeName}</Text>
             <Text style={styles.QuizInformationText}>{quiz.difficulty}</Text>
             {/*<Text style={styles.QuizInformationText}>{quiz.questionCount}</Text>*/}
             <TouchableOpacity style={styles.QuizInformationButton} onPress={handleStartQuiz}>
