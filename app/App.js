b4c9d93b27b3278366346adbab400c5a7f7873b5
behrg <guillaume.behr3@etu.unistra.fr>
2024-11-21 20:31:14 +0100

    add : quiz creations screen to app

--- app/App.js
+++ app/App.js
@@ -9,12 +9,13 @@ import ResumeScreen from './src/screens/ResumeScreen';
 import EndScreen from './src/screens/EndScreen';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
-import AccountInfos from './src/screens/Account';
+import RetrieveQuestions from './src/screens/RetrieveQuestionsScreen';
 
 import '@expo/metro-runtime'
 
 import { Platform } from 'react-native';
 import Account from './src/screens/Account';
+import QuizCreation from './src/screens/QuizCreationScreen';
 
 // Configuration pour le deep linking
 const linking = {
@@ -30,6 +31,12 @@ const linking = {
       userInfos: 'userInfos',
       login: 'login',
       register: 'register',
+      retrieveQuestions: {
+        path: 'retrieveQuestions/:handleAddQuestions',
+        stringify: {
+          handleAddQuestions: (handleAddQuestions) => "",
+        },
+      },
     },
   },
 };
@@ -44,6 +51,7 @@ const MenuDrawer = () => {
       <drawer.Screen name="newQuiz" component={Parameters} options={{ title: "Créer un nouveau QUIZ" }} />
       <drawer.Screen name="resumeQuiz" component={ResumeScreen} options={{ title: "Reprendre un QUIZ" }} />
       <drawer.Screen name="account" component={Account} options={{ title: "Mon compte" }} />
+      {Platform.OS === 'web' ? <drawer.Screen name="quizCreation" component={QuizCreation} options={{ title: "Créer un quiz" }} /> : null}
     </drawer.Navigator>
   );
 };
@@ -57,6 +65,7 @@ const MenuStack = () => {
         <stack.Screen name="endScreen" component={EndScreen} options={{ title: "Résultat", headerLeft: Platform.OS === 'web' ? () => null : undefined }} />
         <stack.Screen name="login" component={Login} options={{ title: "Se connecter", headerLeft: Platform.OS === 'web' ? () => null : undefined}} />
         <stack.Screen name="register" component={Register} options={{ title: "S'inscrire", headerLeft: Platform.OS === 'web' ? () => null : undefined}} />
+        {Platform.OS === 'web' ? <drawer.Screen name="retrieveQuestions" component={RetrieveQuestions} options={{ title: "Récupérer des questions"}} /> : null}
       </stack.Group>
     </stack.Navigator>
   );
