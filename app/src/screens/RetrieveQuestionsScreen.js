a00eb0e8bb2aeaa17855aced0db3a59abaeb3d3d
behrg <guillaume.behr3@etu.unistra.fr>
2024-11-21 20:28:46 +0100

    add : retrieveQuestionsScreen

---
+++ app/src/screens/RetrieveQuestionsScreen.js
@@ -0,0 +1,36 @@
+import { TouchableOpacity, View, Text } from 'react-native';
+import { useState } from 'react';
+import ThemePicker from '../components/ThemePicker';
+import DifficultyRadioSelector from '../components/DifficultyRadioSelector';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { getQuestions } from '../utils/api';
+import RangeCursor from '../components/Cursor';
+
+export default function RetrieveQuestions(){
+    const route = useRoute();
+    const { handleAddQuestions } = route.params;
+
+    const navigation = useNavigation();
+
+    const [amount, setAmount] = useState(1);
+    const [category, setCategory] = useState(9);
+    const [difficulty, setDifficulty] = useState('easy');
+
+    const handleRetrieveQuestions = async () => {
+
+        const questions = await getQuestions(amount, category, difficulty);
+        handleAddQuestions(questions);
+        navigation.goBack();
+    }
+
+    return(
+        <View>
+            <ThemePicker value={category} onValueChange={setCategory}/>
+            <DifficultyRadioSelector value={difficulty} onValueChange={setDifficulty}/>
+            <RangeCursor value={amount} onValueChange={setAmount}/>
+            <TouchableOpacity onPress={handleRetrieveQuestions}>
+                <Text>Valider</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
