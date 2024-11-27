807641f0cd0ca32db05f776364fb55486c311414
WILLEM THEO <theo.willem@etu.unistra.fr>
2024-11-27 16:32:02 +0100

    Update: Mise à jour des composants et du style
    - externalisation du style des boutons

--- app/src/screens/HomeScreen.js
+++ app/src/screens/HomeScreen.js
@@ -1,25 +1,29 @@
-import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-
-const platform = Platform.OS;
+import SimpleButton from '../components/SimpleButton';
+import { loadFont } from '../utils/utils';
+import { COLORS } from '../css/utils/color';
+import { FONT } from '../css/utils/font';
 
 export default function HomeScreen() {
     const navigation = useNavigation();
+    loadFont();
 
     return (
         <View style={styles.homeView}>
-            <Text style={styles.appTitle}>Luoja</Text>
-            <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate('newQuiz')}>
-                <Text style={styles.buttonText}>Générer un Quiz</Text>
-            </TouchableOpacity>
+            <Text style={[FONT.title, {fontFamily:'LobsterTwo_700Bold_Italic'}]}>Luoja</Text>
 
-            <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate('search')}>
-                <Text style={styles.buttonText}>Quiz de la communauté</Text>
-            </TouchableOpacity>
+            <Image source={require('../../assets/splash.png')} />
 
-            <TouchableOpacity style={styles.buttons} onPress={() => navigation.navigate('resumeQuiz')}>
-                <Text style={styles.buttonText}>Reprendre la partie</Text>
-            </TouchableOpacity>
+            <div style={styles.listButton}>
+            <SimpleButton text="Générer un Quiz" onPress={() => navigation.navigate('newQuiz')}/>
+
+            <SimpleButton text="Quiz de la communauté" onPress={() => navigation.navigate('search')} />
+
+            <SimpleButton text="Reprendre la partie" onPress={() => navigation.navigate('resumeQuiz')} />
+            </div>
+
+            <Image source={require('../../assets/splash.png')} />
         </View>
     );
 }
@@ -27,32 +31,15 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     homeView: {
         flex: 1,
-        justifyContent: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
         alignItems: 'center',
-        backgroundColor: '#fff',
+        backgroundColor: COLORS.background.blue,
+        color: COLORS.text.blue.dark,
     },
-    appTitle: {
-        position: 'absolute',
-        top: 100,
-        fontSize: 50,
-        fontWeight: 'bold',
-    },
-    buttons: {
-        display: 'flex',
+    listButton: {
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#8fd3ff',
-        height: 50,
-        width: 250,
-        borderRadius: 15,
-        marginVertical: 10,
-        ...platform === 'web' ? { 
-            boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.25)',
-        } : { elevation: 2 },
-    },
-    buttonText: {
-        fontSize: 20,
-        fontWeight: 'bold',
     },
 });
