49a962dc64f3f50aa9327bcfacf23373260522ca
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-27 16:03:22 +0100

    Update: ajout de du nombre de question et le nombre de partie jouées

--- app/src/components/CreatedQuizInformation.js
+++ app/src/components/CreatedQuizInformation.js
@@ -5,34 +5,39 @@ import { getThemeLabel, getPlatformStyle, formatReadableDate } from '../utils/ut
 
 const styles = getPlatformStyle();
 
-export default function CreatedQuizInformation({quizId, category, difficulty, date, status, title}) {
+export default function CreatedQuizInformation({quizId, category, difficulty, date, status, title, nbQuestions}) {
     const [loading, setLoading] = useState(true);
     const [average, setAverage] = useState("aucune donnée");
     const [themeName, setThemeName] = useState("any");
     const [statusStr, setStatus] = useState("privé");
     const [dateStr, setDate] = useState("any");
+    const [nbPlayed, setNbPlayed] = useState(0);
+    const [nbQuestionsStr, setNbQuestions] = useState("any");
     useEffect(() => {
         async function fetchParty() {
             setLoading(false);
             if (category !== 0){
                 setThemeName(getThemeLabel(category));
             }
-            getQuizAverage(quizId).then((score) => {
-                if (score.score === null){
-                    score.score = "aucune donnée";
+            getQuizAverage(quizId).then((data) => {
+                console.log(data);
+                if (data.score === null){
+                    data.score = "aucune donnée";
                 } else {
-                    setAverage(score.score);
+                    setAverage(data.score + "%");
+                    
                 }
-                
+                setNbPlayed(data.nombreDePartie);
             });
             if(status === "true"){
                 setStatus("public");
             }
             let dateTemp = formatReadableDate(date);
             setDate(dateTemp);
+            setNbQuestions(nbQuestions);
         }
         fetchParty();
-    }, [category, status, quizId, date]);
+    }, [category, status, quizId, date, nbQuestions]);
 
     if (loading) {
         return <Text>Chargement...</Text>;
@@ -47,7 +52,10 @@ export default function CreatedQuizInformation({quizId, category, difficulty, da
             <Text style={styles.historyQuizInformationText}>{difficulty}</Text>
             <Text style={styles.historyQuizInformationText}>{statusStr}</Text>
             <Text style={styles.historyQuizInformationText}>{dateStr}</Text>
-            <Text style={styles.historyQuizInformationText}>Pourcentage de réussite : {average}</Text>
+            <Text style={styles.historyQuizInformationText}>Nombre de questions : {nbQuestionsStr}</Text>
+            <Text style={styles.historyQuizInformationText}>Réussite : {average}</Text>
+            <Text style={styles.historyQuizInformationText}>Nombre de parties jouées : {nbPlayed}</Text>
+
         </View>
     );
 }
