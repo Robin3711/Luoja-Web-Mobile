73e4345c35291adf31689655e5970a2357b67b80
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-11-29 12:07:40 +0100

    update: remove unuse variable

--- app/src/components/CreatedQuizInformation.js
+++ app/src/components/CreatedQuizInformation.js
@@ -5,7 +5,6 @@ import { getQuizAverage } from '../utils/api';
 export default function CreatedQuizInformation({quizId, category, difficulty, date, status, title, nbQuestions}) {
     const [loading, setLoading] = useState(true);
     const [average, setAverage] = useState("aucune donnée");
-    const [statusStr, setStatus] = useState("privé");
     const [nbPlayed, setNbPlayed] = useState(0);
     const [nbQuestionsStr, setNbQuestions] = useState("any");
     useEffect(() => {
@@ -20,9 +19,7 @@ export default function CreatedQuizInformation({quizId, category, difficulty, da
                 }
                 setNbPlayed(data.nombreDePartie);
             });
-            if(status === true){
-                setStatus("public");
-            }
+
             setNbQuestions(nbQuestions);
         }
         fetchParty();
