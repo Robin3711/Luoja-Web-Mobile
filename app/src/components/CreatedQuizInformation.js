75afeaf25f01954fb6e0d5f59aa3e91e3cfcf332
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2024-12-04 13:55:33 +0100

    update: Couleur du background du composant en blanc

--- app/src/components/CreatedQuizInformation.js
+++ app/src/components/CreatedQuizInformation.js
@@ -103,6 +103,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 4,
         elevation: 2, // Ombre sur Android
+        backgroundColor: 'white',
     },
     PrincipalInformationsView: {
         display: 'flex',
