48fa0687d46cba3b082f4f9b1184fe37c7b1ce23
WILLEM THEO <theo.willem@etu.unistra.fr>
2024-12-04 15:21:38 +0100

    fix: MAJ du nom de génération de quiz rapide

--- app/App.js
+++ app/App.js
@@ -60,10 +60,10 @@ const MenuDrawer = () => {
   return (
     <drawer.Navigator screenOptions={{ drawerPosition: 'left' }} initialRouteName='home'>
       <drawer.Screen name="home" component={HomeScreen} options={{ title: "Luoja", drawerLabel: "Accueil" }} />
-      <drawer.Screen name="newQuiz" component={Parameters} options={{ title: "Luoja", drawerLabel: "Générer un QUIZ" }} />
+      <drawer.Screen name="newQuiz" component={Parameters} options={{ title: "Luoja", drawerLabel: "Quiz rapide" }} />
       <drawer.Screen name="search" component={SearchScreen} options={{ title: "Luoja", drawerLabel: "Quiz de la communauté" }} />
       <drawer.Screen name="resumeQuiz" component={ResumeScreen} options={{ title: "Luoja", drawerLabel: "Reprendre la partie" }} />
-      {Platform.OS === 'web' ? <drawer.Screen name="quizCreation" component={QuizCreation} options={{ title: "Luoja", drawerLabel: "Créer votre propre QUIZ" }} /> : null}
+      {Platform.OS === 'web' ? <drawer.Screen name="quizCreation" component={QuizCreation} options={{ title: "Luoja", drawerLabel: "Créer votre propre quiz" }} /> : null}
       <drawer.Screen name="account" component={Account} options={{ title: "Luoja", drawerLabel: "Tableau de bord" }} />
     </drawer.Navigator>
   );
