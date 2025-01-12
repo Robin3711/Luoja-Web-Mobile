fd9d5775d34c0a44277d5ec1157f7b4c9509aaaf
WILLEM THEO <theo.willem@etu.unistra.fr>
2025-01-12 15:45:36 +0100

    fix: center button text on mobile

--- app/src/components/SimpleButton.js
+++ app/src/components/SimpleButton.js
@@ -4,23 +4,43 @@ import { FONT } from '../css/utils/font';
 
 const platform = Platform.OS;
 
-export default function SimpleButton({ text, onPress, color = COLORS.button.blue.basic, height = 75, width = 350, textStyle = {} }) { 
+export default function SimpleButton({
+    text,
+    onPress,
+    color = COLORS.button.blue.basic,
+    height = 75,
+    width = 350,
+    textStyle = {}
+}) {
     return (
-        <TouchableOpacity onPress={onPress} style={[styles.button, { backgroundColor: color, height: height, width: width }]} >
-            <Text style={[FONT.button, textStyle]}>{text}</Text>
+        <TouchableOpacity
+            onPress={onPress}
+            style={[styles.button, { backgroundColor: color, height, width }]}
+        >
+            <Text style={[FONT.button, { textAlign: 'center' }, textStyle]}>{text}</Text>
         </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
     button: {
-        position: 'relative', // Permet de positionner le texte absolument par rapport au bouton
+        position: 'relative',
         backgroundColor: COLORS.button.blue.basic,
         borderRadius: 15,
         marginVertical: 10,
         marginBottom: 25,
-        ...platform === 'web' ? {
-            boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.25)',
-        } : { elevation: 2 },
+        justifyContent: 'center', // Centre le contenu verticalement
+        alignItems: 'center',    // Centre le contenu horizontalement
+        ...platform === 'web'
+            ? {
+                boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.25)',
+            }
+            : {
+                elevation: 2, // Ombres pour Android
+                shadowColor: '#000', // Ombres pour iOS
+                shadowOffset: { width: 0, height: 1 },
+                shadowOpacity: 0.25,
+                shadowRadius: 1,
+            },
     },
 });
