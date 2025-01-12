d400689d7393e0ba1e03271d8c57e5ce6a83ac63
Matteo Gillig <matteo.gillig@etu.unistra.fr>
2025-01-12 08:16:34 +0100

    add: Composant pour rejoindre une partie
    Le composant va faire la demande de permission pour la caméra puis l'ouvrir pour scanner le QR  code, une fois fait on navigue vers la room

---
+++ app/src/screens/JoinRoom.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, Alert } from "react-native";
+import { CameraView, Camera } from "expo-camera";
+import { getRoomId, hasToken } from "../utils/utils";
+import { useNavigation } from "@react-navigation/native";
+
+export default function joinGame() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!hasToken()) {
+        navigation.navigate('login');
+    };
+    const getCameraPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getCameraPermissions();
+  }, []);
+
+  const handleBarcodeScanned = ({ type, data }) => {
+    setScanned(true);
+    if (type === "qr")
+         {
+            const roomId = getRoomId(data);
+            Alert.alert(
+                "Code détectée",
+                `Voulez-vous rejoindre cette partie ?\n${roomId}`,
+                [
+                { text: "Annuler", style: "cancel", onPress: () => setScanned(false) },
+                { text: "Ouvrir", onPress: () =>  navigation.navigate("room", { roomId : roomId }) },
+                ]
+            );
+        }
+
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <CameraView
+        onBarcodeScanned={scanned ? undefined : handleBarcodeScanned}
+        barcodeScannerSettings={{
+          barcodeTypes: ["qr", "pdf417"],
+        }}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
