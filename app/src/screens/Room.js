c96289098eba5a028bfe08b4000d6b7c6bc6e31c
WILLEM THEO <theo.willem@etu.unistra.fr>
2025-01-12 13:42:08 +0100

    add : CSS to the room screen

--- app/src/screens/Room.js
+++ app/src/screens/Room.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 
 import { joinRoom } from "../utils/api";
 import { getPlatformAPI } from "../utils/utils";
 
 import QRCode from "react-native-qrcode-svg";
+import { COLORS } from "../css/utils/color";
+import { FONT } from "../css/utils/font";
 
 export default function Room() {
     const route = useRoute();
@@ -53,20 +55,39 @@ export default function Room() {
     }, []);
 
     return (
-        <View>
-            <Text>Room {roomId}</Text>
-            <Text>Players :</Text>
-            <View>
-                {players.map((player) => (
-                    <Text key={player}>{player}</Text>
-                ))}
+        <View style={styles.container}>
+            <Text style={[FONT.title, styles.title]}>Room {roomId}</Text>     
+            <View>    
+                <Text style={FONT.text}>Players :</Text>
+                <View style={FONT.text}>
+                    {players.map((player) => (
+                        <Text key={player} style={FONT.text}>{player}</Text>
+                    ))}
+                </View>
+            </View>   
+            <View style={{ borderWidth: 10 , borderColor: COLORS.palette.blue.darker, borderRadius: 30, padding: 10, backgroundColor: 'white' }}>
+                <QRCode
+                    value={`${apiUrl}/room/${roomId}/join`}
+                    size={200}
+                    color= {COLORS.palette.blue.darker}
+                    backgroundColor="white"
+                />
             </View>
-            <QRCode
-                value={`${apiUrl}/room/${roomId}/join`}
-                size={200}
-                color="black"
-                backgroundColor="white"
-            />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: COLORS.background.blue,
+        gap: 50,
+    },
+    title: {
+        position: 'absolute',
+        top: 50,
+    },
+});
