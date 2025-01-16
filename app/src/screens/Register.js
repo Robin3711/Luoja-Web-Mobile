1bd57bc7bf72065dfcc94b3f13ecef16a5584aaf
behrg <guillaume.behr3@etu.unistra.fr>
2025-01-16 21:26:43 +0100

    update : taille name login et register

--- app/src/screens/Register.js
+++ app/src/screens/Register.js
@@ -134,7 +134,7 @@ const styles = StyleSheet.create({
     },
     registerInput: {
         height: 40,
-        width: 250,
+        width: 280,
         margin: 12,
         borderWidth: 1,
         padding: 10,
